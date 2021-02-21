b9b3204437080d491b2b7546b71db12939fa5be2
Yuliya Kryvorot <krviua@yahoo.com>
2021-02-21 11:57:22 +0300

    feat: implement card component

---
+++ memory-game/src/components/Card/Card.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import cobweb from './cobweb.png';
+import owl from './owl.png';
+import broom from './broom.png';
+import witchHat from './witch-hat.png';
+
+import './Card.css';
+
+export default class Card extends Component {
+  render() {
+    const { name, imageUrl, isFlipped } = this.props;
+
+    return (
+      <div className={`card ${isFlipped ? 'visible' : ''}`}>
+        <div className='card-back card-face'>
+          <img src={cobweb} alt='' className='cobweb top-left' />
+          <img src={cobweb} alt='' className='cobweb top-right' />
+          <img src={cobweb} alt='' className='cobweb bottom-left' />
+          <img src={cobweb} alt='' className='cobweb bottom-right' />
+          <img src={broom} alt='' className='broom' />
+        </div>
+        <div
+          className='card-front card-face'
+          style={{ backgroundImage: `url(/images/${imageUrl})` }}>
+          <div className='scroll-name card-face'>
+            <p>{name}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
