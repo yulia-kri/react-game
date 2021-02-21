ee57b574d98084b3970ab9be1553c3d9c1313d13
Yuliya Kryvorot <krviua@yahoo.com>
2021-02-21 11:56:58 +0300

    feat: add cards data

---
+++ memory-game/src/data/cards.data.js
@@ -0,0 +1,52 @@
+const cards = [
+  {
+    imageUrl: 'AlbusDumbledore.jpg',
+    name: 'Albus Dumbledore',
+  },
+  {
+    imageUrl: 'MinervaMcGonagall.jpg',
+    name: 'Minerva McGonagall',
+  },
+  {
+    imageUrl: 'SeverusSnape.jpg',
+    name: 'Severus Snape',
+  },
+  {
+    imageUrl: 'RubeusHagrid.jpg',
+    name: 'Rubeus Hagrid',
+  },
+  {
+    imageUrl: 'HarryPotter.jpg',
+    name: 'Harry Potter',
+  },
+  {
+    imageUrl: 'HermioneGranger.jpg',
+    name: 'Hermione Granger',
+  },
+  {
+    imageUrl: 'RonWeasley.webp',
+    name: 'Ron Weasley',
+  },
+  {
+    imageUrl: 'Voldemort.jpg',
+    name: 'Lord Voldemort',
+  },
+  {
+    imageUrl: 'DracoMalfoy.jpg',
+    name: 'Draco Malfoy',
+  },
+  {
+    imageUrl: 'LunaLovegood.jpg',
+    name: 'Luna Lovegood',
+  },
+  {
+    imageUrl: 'NevilleLongbottom.png',
+    name: 'Neville Longbottom',
+  },
+  {
+    imageUrl: 'Dobby.jpg',
+    name: 'Dobby',
+  },
+];
+
+export default cards;
