c58ece644ff63f7ab9a3dd5a712b39c293025f43
Yuliya Kryvorot <krviua@yahoo.com>
2021-02-22 15:34:29 +0300

    feat: add spinner

---
+++ memory-game/src/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Spinner.css';
+
+const Spinner = () => (
+  <div className='lds-ripple'>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default Spinner;
