3cf3ee3dfabe3456208c2a8477ca992641948db9
Yuliya Kryvorot <krviua@yahoo.com>
2021-02-22 14:45:34 +0300

    feat: add basic styles

--- memory-game/src/components/App/App.js
+++ memory-game/src/components/App/App.js
@@ -7,18 +7,8 @@ import './App.css';
 const App = () => {
   return (
     <React.Fragment>
-      <h1 className='page-title'>Harry Potter Memory Game</h1>
-      <div className='game'>
-        <div className='game__info'>
-          <div className='info__time'>
-            Time <span>100</span>
-          </div>
-          <div className='info__flips'>
-            Flips <span>10</span>
-          </div>
-        </div>
-        <Game />
-      </div>
+      <h1 className='page-title'>Memory Game in Hogwarts</h1>
+      <Game totalTime={100} numberOfCards={8} cardBack={'hat'} />
     </React.Fragment>
   );
 };
